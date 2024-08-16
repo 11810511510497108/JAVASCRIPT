c0650661d8066dc01b40f99abce84597ccbccb53
vishal patidar <136967663+11810511510497108@users.noreply.github.com>
2024-08-16 11:12:09 +0000

    String js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,41 @@
+const name = "vishal";
+
+//console.log(name);
+
+const fullName = new String('vishal-patidar-suman');
+
+// console.log(fullName)
+
+// console.log(name + fullName);
+
+// console.log(`My name is ${name} and my full name is ${fullName}`);
+
+// console.log(typeof name);
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.length);
+// console.log(fullName.charAt(3));
+
+// console.log(fullName.indexOf('a'));
+
+// console.log(name.concat(fullName));
+// console.log(name.fixed());
+
+// console.log(name.lastIndexOf(2,'a'));
+
+// const str = fullName.substring(1,6);
+// console.log(str);
+
+// const read = "    java    script.  ";
+// console.log(read);
+// console.log(read.trim());
+// console.log(read.trimEnd());
+
+// const url = "http//vishalptdr/patidar%200vishal";
+
+// console.log(url.replace("%200","---"));
+
+// console.log(url.includes("patidar"));
+
+//console.log(fullName.split("-"));
